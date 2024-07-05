dbbcbf8682712a32ab35b5b805c0ec87e85c1f87
Owen Yang <owentheyanger@gmail.com>
2024-07-05 11:54:43 -0700

    added transaction resolvers

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,68 @@
+import Transaction from "../models/transaction.model";
+
 const transactionResolver = {
-    Query: {},
-    Mutation: {},
+    Query: {
+        transactions: async (_, __, context) => {
+            try{
+                if(!context.getUser()){
+                    throw new Error ("Unauthorized");
+                }
+                const userId = await context.getUser()._id;
+                const transactions = await Transaction.find({user: userId});
+                return transactions;
+            }
+        catch(error){
+            console.error("Error getting transactions: ", err);
+            throw new Error("Error getting transactions");
+
+        }
+        },
+        transaction: async(_,{transactionId}, __) =>{
+            try{
+                const transaction = await Transaction.findById(transactionId);
+                return transaction
+            }
+            catch(error){
+                console.error("Error getting transaction: ", err)
+                throw new Error("Error getting transaction");
+            }
+        },
+    },
+    Mutation: {
+        createTransaction: async(parent, {input}, context) =>{
+            try{
+                const newTransaction = new Transaction({
+                    ...input,
+                    userId:context.getUser()._id
+                })
+                await newTransaction.save() // saves to database
+                return newTransaction
+            }
+            catch(err){
+                console.error("Error creating transaction: ", err)
+                throw new Error("Error creating transaction");
+            }
+        },
+        updateTransaction: async(_, {input},_) =>{
+            try{
+                // new returns the updated document to the database
+                const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {new:true})
+                return updatedTransaction
+            }
+            catch(err){
+                console.error("Error updating transaction: ", err)
+                throw new Error("Error updating transaction");
+            }
+        },
+        deleteTransaction: async(_, {transactionId}, _) =>{
+            try{
+                const deleteTransaction = await Transaction.findByIdAndDelete(transactionId);
+                return deleteTransaction;
+            }catch(err){
+                console.error("Error deleting transaction: ", err)
+                throw new Error("Error deleting transaction");
+            }
+        }
+    },
 }
 export default transactionResolver;
